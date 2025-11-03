cf7ce1a63b26112172a1066694e6435fc4be7d96
Tanish <tanishpal5555@gmail.com>
2025-11-03 22:39:03 +0530

    phase 3.5

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,3 @@
-// import { MongoClient } from "mongodb";
-
-// const uri = "mongodb://127.0.0.1:27017";
-// const client = new MongoClient(uri);
-
-// async function connectDB() {
-//   try {
-//     await client.connect();
-//     console.log("✅ MongoDB connected!");
-//     return client.db("chessDB");
-//   } catch (err) {
-//     console.error("❌ MongoDB connection failed:", err);
-//   }
-// }
-
-// export default connectDB;
-
-
 
 import { MongoClient } from "mongodb";
 
@@ -39,28 +21,3 @@ async function connectDB() {
 }
 
 export default connectDB;
-
-
-
-
-
-
-
-
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`❌ Error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
--- frontend/src/views/Common/Header.jsx
+++ frontend/src/views/Common/Header.jsx
@@ -1,5 +1,4 @@
 
-
 import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
